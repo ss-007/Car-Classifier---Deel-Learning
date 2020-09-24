a0668feece4b18dc0a06f1ec055b070061d82898
Sacchit Shah <skatersacchit@gmail.com>
2020-09-25 00:27:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH ='model_resnet50.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    ## Scaling
+    x=x/255
+    x = np.expand_dims(x, axis=0)
+   
+
+    x = preprocess_input(x)
+
+    preds = model.predict(x)
+    preds=np.argmax(preds, axis=1)
+    if preds==0:
+        preds="Audi it Is"
+    elif preds==1:
+        preds="The Car is Lamborghini"
+    else:
+        preds="It is Mercedes"
+    
+    
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        preds = model_predict(file_path, model)
+        result=preds
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
